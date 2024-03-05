aa964ec5f1f536796326478241ea8f141ae74408
farahAllouche <farahallouche327@gmail.com>
2024-03-06 00:34:55 +0100

    add calendar

--- src/pages/DeclFisc.jsx
+++ src/pages/DeclFisc.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import DFCalendar from "../components/DFCalendar";
 
 export default function DeclFisc() {
-  return <div>Decl-Fisc</div>;
+  return (
+    <div className="home-container">
+      <div className="header-container">
+        <div className="header-container-img">
+          <img src="/src/assets/header/decFisc2.svg" />
+        </div>
+      </div>
+      <div>
+        <DFCalendar />
+      </div>
+    </div>
+  );
 }
